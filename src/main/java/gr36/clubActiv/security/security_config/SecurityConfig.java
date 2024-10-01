d2ba97989e184399ff2a4492e0ff86aa020edabd
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-10-01 22:41:16 +0200

    update methods for user-activities

--- src/main/java/gr36/clubActiv/security/security_config/SecurityConfig.java
+++ src/main/java/gr36/clubActiv/security/security_config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
             .requestMatchers(HttpMethod.PUT, "/api/activity/update/{id}").hasAnyRole("ADMIN", "USER")
             .requestMatchers(HttpMethod.DELETE, "/api/activity/{id}").hasAnyRole("ADMIN", "USER")
             .requestMatchers(HttpMethod.POST, "/api/activity/{activity_id}/add-user/{user_id}").hasAnyRole("ADMIN", "USER")
+            .requestMatchers(HttpMethod.GET, "/api/activity/user/{userId}/activities").hasAnyRole("ADMIN", "USER")
             .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/refresh").permitAll() // разрешаем всем доступ к этим endpoints
             .requestMatchers(HttpMethod.POST, "/register").permitAll()
             .requestMatchers(HttpMethod.GET, "/register").permitAll()
