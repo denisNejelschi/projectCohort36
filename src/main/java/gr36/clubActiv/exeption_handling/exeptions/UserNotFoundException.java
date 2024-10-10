740220bf31f8a16afa2a0c61a78f30179009afea
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-10-11 00:02:15 +0200

    add exeption RoleNotFoundExeption

--- src/main/java/gr36/clubActiv/exeption_handling/exeptions/UserNotFoundException.java
+++ src/main/java/gr36/clubActiv/exeption_handling/exeptions/UserNotFoundException.java
@@ -3,12 +3,10 @@ package gr36.clubActiv.exeption_handling.exeptions;
 public class UserNotFoundException extends RuntimeException {
 
   public UserNotFoundException(Long userId) {
-    super(String.format("User with id %d not found",  userId));
+    super(String.format("User with id %d not found", userId));
   }
+
   public UserNotFoundException(String message) {
-    super(String.format("User with  not found"));
+    super(message); // Используем переданное сообщение, как и должно быть
   }
 }
-
-
-//works !
