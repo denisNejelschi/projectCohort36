49e311fcdcfdb56d215b1adab91610cf6b203fe5
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-10-16 22:48:27 +0200

    update

--- src/main/java/gr36/clubActiv/controller/ActivityController.java
+++ src/main/java/gr36/clubActiv/controller/ActivityController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/activity")
 public class ActivityController {
